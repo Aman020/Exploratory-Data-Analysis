8170d2d4c7166aa64225246903e9523a22a2bc9d
a_m_a_n <amanbhayana@yahoo.in>
2019-03-03 00:07:33 -0500

    Cosmetic changes

--- Lab1Part3.R
+++ Lab1Part3.R
@@ -67,6 +67,46 @@ mergeGeocode<-function(...){
    return( geocode)
 }
 
+plotHeatMap <- function(data,location, frequency)
+{
+  # Reference -https://stackoverflow.com/questions/29614972/ggplot-us-state-map-colors-are-fine-polygons-jagged-r
+  
+  library(ggmap)
+ 
+  library(maps)
+  library(sp)    
+  library(maptools)
+  library(ggplot2)
+  library(maps)
+  library(RColorBrewer)
+  us <- map_data("state")
+  
+  gg <- ggplot()
+  gg <- gg + geom_map(data=us, map=us,aes(x = long, y = lat, map_id=region),
+                      fill="#ffffff", color="#ffffff", size=1)
+  gg <- gg + geom_map(data=data, map=us,
+                      aes(fill=frequency, map_id=location),
+                      color="#fddfff", size=0.15)
+  gg <- gg + labs(x="", y="", fill ="Frequency", title = "2019 Infulenza and Flu Tweets Frequency")
+  #gg <- gg + coord_map("albers", lat0 = 39, lat1 = 45) 
+  gg<-gg + scale_fill_gradient(name="Flu",low='white', high='red') +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(legend.position=c("0.9", "0.2"))
+  gg <- gg + coord_map("albers", lat0 = 39, lat1 = 45) 
+  gg <- gg + theme(axis.ticks = element_blank())
+  gg
+}
+getStateFrequency<-function(geocodes)
+{
+  dfGeodata<-data.frame( geocodes$lon, geocodes$lat)
+
+  usStatelocation <- latlong2state(dfGeodata)
+  US_stateFreq <- table(usStatelocation)
+  df_US_state_frequency <- data.frame(US_stateFreq)
+  return(df_US_state_frequency)
+}
+
+
 tweetsFlu <- getAndSaveTweets("flu",15000,twitter_token,"Twitter_data/raw_15k_flu_2mar.csv" )
 tweetsHashFlu <- getAndSaveTweets("#flu",15000,twitter_token,"Twitter_data/raw_15k_HashFlu_2mar.csv" )
 tweetsInfluenza <- getAndSaveTweets("influenza",15000,twitter_token,"Twitter_data/raw_15k_influenza_2mar.csv" )
@@ -76,31 +116,24 @@ cleanTweetsFlu <- cleanAndSaveGeocodedTweets(tweetsFlu,"Twitter_data/clean_15k_f
 cleanTweetsHashFlu <- cleanAndSaveGeocodedTweets(tweetsHashFlu,"Twitter_data/clean_15k_hash_flu_2mar.csv")
 cleanTweetsInfluenza <- cleanAndSaveGeocodedTweets(tweetsInfluenza,"Twitter_data/clean_15k_influenza_2mar.csv")
 
+#-----------------Plotting geocodes of  all the tweets collected using search query as flu----------
+dfGeodataTweetsFluStateFreq<-getStateFrequency(cleanTweetsFlu)
+plotHeatMap(dfGeodataTweetsFluStateFreq,dfGeodataTweetsFluStateFreq$usStatelocation,dfGeodataTweetsFluStateFreq$Freq)
 
-geodata <- mergeGeocode(cleanTweetsFlu,cleanTweetsInfluenza,cleanTweetsHashFlu)
-dfGeodata<-data.frame( geodata$lon, geodata$lat)
-write.csv(dfGeodata, file = "/Users/aman/R/Lab1EDA/Twitter_data/Geocodes.csv",row.names=FALSE, na="")
-head(geodata)
-usStatelocation <- latlong2state(dfGeodata)
-US_stateFreq <- table(usStatelocation)
-df_US_state_frequency <- data.frame(US_stateFreq)
-df_US_state_frequency
-write.csv(df_US_state_frequency, file = "/Users/aman/R/Lab1EDA/Twitter_data/US_State_Frequency_2mar.csv",row.names=FALSE, na="")
-us <- map_data("state")
 
-# Reference -https://stackoverflow.com/questions/29614972/ggplot-us-state-map-colors-are-fine-polygons-jagged-r
+#-----------------Plotting geocodes of  all the tweets collected using search query as #flu----------
 
-gg <- ggplot()
-gg <- gg + geom_map(data=us, map=us,aes(x = long, y = lat, map_id=region),
-                    fill="#ffffff", color="#ffffff", size=1)
-gg <- gg + geom_map(data=df_US_state_frequency, map=us,
-                    aes(fill=df_US_state_frequency$Freq, map_id=df_US_state_frequency$usStatelocation),
-                    color="#fddfff", size=0.15)
-gg <- gg + labs(x="", y="", fill ="Frequency", title = "2019 Infulenza and Flu Tweets Frequency")
-#gg <- gg + coord_map("albers", lat0 = 39, lat1 = 45) 
-gg<-gg + scale_fill_gradient(name="Flu",low='white', high='red',limits=c(0,700)) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(legend.position=c("0.9", "0.2"))
-gg <- gg + coord_map("albers", lat0 = 39, lat1 = 45) 
-gg <- gg + theme(axis.ticks = element_blank())
-gg
+dfGeodataTweetsHashFluStateFreq<-getStateFrequency(cleanTweetsHashFlu)
+plotHeatMap(dfGeodataTweetsHashFluStateFreq,dfGeodataTweetsHashFluStateFreq$usStatelocation,dfGeodataTweetsHashFluStateFreq$Freq)
+
+#-------------------Plotting geocodes of  all the tweets collected using search query as influenza----------
+dfGeodataTweetsInfluenzaStateFreq<-getStateFrequency(cleanTweetsInfluenza)
+plotHeatMap(dfGeodataTweetsInfluenzaStateFreq,dfGeodataTweetsInfluenzaStateFreq$usStatelocation,dfGeodataTweetsInfluenzaStateFreq$Freq)
+
+
+
+#-------------------- Plotting all the geocodes of all the tweets collected-------------------
+allGeodata <- mergeGeocode(cleanTweetsFlu,cleanTweetsInfluenza,cleanTweetsHashFlu)
+allGeodataStateFreq<-getStateFrequency(allGeodata)
+write.csv(allGeodataStateFreq, file = "/Users/aman/R/Lab1EDA/Twitter_data/US_State_Frequency_2mar.csv",row.names=FALSE, na="")
+plotHeatMap(allGeodataStateFreq,allGeodataStateFreq$usStatelocation,allGeodataStateFreq$Freq)
